c058e1ec819b8f9536cd1fc4e14a851af945be39
jos3lo89 <gothangel21@hotmail.com>
2024-02-25 09:44:11 -0500

    css example

---
+++ cssexample/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BootstrapComponent from "./components/BootstrapComponent.vue";
+</script>
+
+<template>
+  <BootstrapComponent></BootstrapComponent>
+</template>
+
+<style scoped></style>
---
+++ cssexample/src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// main.js
+
+// Importar estilos de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importar JavaScript de Bootstrap
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// Aquí continúa tu código de Vue.js
+
+
+createApp(App).mount('#app')
